45b1fe60aaf4a1cf9b981db8110d339f4e41c04b
nv6281-cmd <nv6281@srmist.edu.in>
2026-02-16 20:50:04 +0530

    UC1: Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
