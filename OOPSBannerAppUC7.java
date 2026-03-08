cd89e24f018b3932d09509f5553d3aaf6c80947c
nv6281-cmd <nv6281@srmist.edu.in>
2026-03-08 22:30:41 +0530

    Added UC7 OOPS Banner App

--- OOPSBannerAppUC7.java
+++ OOPSBannerAppUC7.java
@@ -1,102 +1,92 @@
-/**
- * OOPS Banner App - UC7
- * Demonstrates storing character patterns using an inner class.
- */
-
 public class OOPSBannerAppUC7 {
 
-    /**
-     * Inner Static Class to store character and pattern
-     */
-    static class CharacterPattern {
+    static class CharacterPatternMap {
 
         private char character;
         private String[] pattern;
 
-        /**
-         * Constructor
-         */
-        public CharacterPattern(char character, String[] pattern) {
+        public CharacterPatternMap(char character, String[] pattern) {
             this.character = character;
             this.pattern = pattern;
         }
 
-        /**
-         * Getter for character
-         */
         public char getCharacter() {
             return character;
         }
 
-        /**
-         * Getter for pattern
-         */
         public String[] getPattern() {
             return pattern;
         }
     }
 
-    /**
-     * Utility method to get pattern for a character
-     */
-    public static String[] getCharacterPattern(char ch, CharacterPattern[] patterns) {
+    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] patterns) {
 
-        for (CharacterPattern cp : patterns) {
+        for (CharacterPatternMap cp : patterns) {
             if (cp.getCharacter() == ch) {
                 return cp.getPattern();
             }
         }
+
         return null;
     }
 
     public static void main(String[] args) {
 
-        CharacterPattern[] patterns = {
+        String[] O = {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
 
-            new CharacterPattern('O', new String[]{
-                    " *** ",
-                    "*   *",
-                    "*   *",
-                    "*   *",
-                    "*   *",
-                    "*   *",
-                    " *** "
-            }),
+        String[] P = {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
 
-            new CharacterPattern('P', new String[]{
-                    "**** ",
-                    "*   *",
-                    "*   *",
-                    "**** ",
-                    "*    ",
-                    "*    ",
-                    "*    "
-            }),
+        String[] S = {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
 
-            new CharacterPattern('S', new String[]{
-                    " ****",
-                    "*    ",
-                    "*    ",
-                    " *** ",
-                    "    *",
-                    "    *",
-                    "**** "
-            })
+        CharacterPatternMap[] patterns = {
+            new CharacterPatternMap('O', O),
+            new CharacterPatternMap('P', P),
+            new CharacterPatternMap('S', S)
         };
 
         String word = "OOPS";
 
+        StringBuilder[] banner = new StringBuilder[7];
+
         for (int i = 0; i < 7; i++) {
+            banner[i] = new StringBuilder();
+        }
 
-            StringBuilder line = new StringBuilder();
+        for (char ch : word.toCharArray()) {
 
-            for (char ch : word.toCharArray()) {
+            String[] pattern = getCharacterPattern(ch, patterns);
 
-                String[] pattern = getCharacterPattern(ch, patterns);
-                line.append(pattern[i]).append("  ");
+            for (int i = 0; i < 7; i++) {
+                banner[i].append(pattern[i]).append("  ");
             }
+        }
 
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(banner[i]);
         }
     }
 }
